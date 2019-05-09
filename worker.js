197c37eff6c49455a4e16c063cc335f868fed6aa
Enbo Chen <aprilsnows@users.noreply.github.com>
2019-05-09 15:39:45 +0300

    fix typo

--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
 let workers = process.env.WEB_CONCURRENCY || 2;
 
-// The maxium number of jobs each worker should process at once. This will need
+// The maximum number of jobs each worker should process at once. This will need
 // to be tuned for your application. If each job is mostly waiting on network 
 // responses it can be much higher. If each job is CPU-intensive, it might need
 // to be much lower.
