3034f24bf3783a70387b111ed082818c2efa2a0f
roggervalf <rogger.valverde@uni.pe>
2023-09-02 09:18:13 -0500

    feat(bullmq): use bullmq example

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express');
-let Queue = require('bull');
+//let Queue = require('bull');
+const {Queue, QueueEvents} = require('bullmq');
 
 // Serve on PORT on Heroku and on localhost:5000 locally
 let PORT = process.env.PORT || '5000';
@@ -9,7 +10,9 @@ let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 let app = express();
 
 // Create / Connect to a named work queue
-let workQueue = new Queue('work', REDIS_URL);
+//let workQueue = new Queue('work', REDIS_URL);
+let workQueue = new Queue('work', { host: 'localhost', port: 6379 });
+let queueEvents = new QueueEvents('work', { host: 'localhost', port: 6379 });
 
 // Serve the two static assets
 app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }));
@@ -20,7 +23,8 @@ app.post('/job', async (req, res) => {
   // This would be where you could pass arguments to the job
   // Ex: workQueue.add({ url: 'https://www.heroku.com' })
   // Docs: https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#queueadd
-  let job = await workQueue.add();
+  //let job = await workQueue.add();
+  let job = await workQueue.add('test', {});
   res.json({ id: job.id });
 });
 
@@ -33,15 +37,17 @@ app.get('/job/:id', async (req, res) => {
     res.status(404).end();
   } else {
     let state = await job.getState();
-    let progress = job._progress;
+    //let progress = job._progress;
+    let progress = job.progress;
     let reason = job.failedReason;
     res.json({ id, state, progress, reason });
   }
 });
 
 // You can listen to global events to get notified when jobs are processed
-workQueue.on('global:completed', (jobId, result) => {
-  console.log(`Job completed with result ${result}`);
+//workQueue.on('global:completed', (jobId, result) => {
+queueEvents.on('completed', ({jobId, returnvalue}) => {
+  console.log(`Job completed with result ${returnvalue}`);
 });
 
 app.listen(PORT, () => console.log("Server started!"));
